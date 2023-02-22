31151a25b72409410f5356bd97c5d69536f8a6d6
m-ajioka <ajiokamine@gmail.com>
2023-02-22 01:30:40 +0000

    Kadai04 routing-

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add');
 });
+
+Route::controller(AAAController::class)->group(function() {
+    Route::get('XXX', 'bbb');
+});
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
